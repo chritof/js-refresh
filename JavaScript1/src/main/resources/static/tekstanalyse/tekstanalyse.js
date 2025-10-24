e87975cad1fa5c99eac13fd661a5ab698950102a
christoffertoft13 <chrtof13@gmail.com>
2025-10-24 12:36:11 +0200

    test

--- JavaScript1/src/main/resources/static/tekstanalyse/tekstanalyse.js
+++ JavaScript1/src/main/resources/static/tekstanalyse/tekstanalyse.js
@@ -5,12 +5,23 @@ class TekstAnalyse{
     #tekst
     #liste = [];
     constructor(){}
-    
+
     set tekst(tekst){
         this.#tekst = tekst;
     }
 
     get liste(){
+        this.#liste = this.#tekst.split(" ")
+        console.log(`Liste: ${this.#liste}`);
         return this.#liste;
     }
+
+    get antallOrd(){
+        
+    }
+
 }
+
+const analyse = new TekstAnalyse();
+analyse.tekst = "Velkommen, ja velkommen til eksamen.";
+analyse.liste;
